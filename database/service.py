658d3e36a041eb5089c96da4028a0f174b3eaf59
João Silva <deb@debian>
2016-07-12 13:58:07 +0100

    Correct typo bug

--- database/service.py
+++ database/service.py
@@ -19,7 +19,7 @@ from .nap import NAP
 from .interface import Interface
 from .port import Port
 from sqlalchemy.exc import IntegrityError
-=`=jedi=0, =`=     (start, *_*stop=None*_*, step=1) =`=jedi=`=
+
 available_vlans = set(range(400, 501))
 
 
